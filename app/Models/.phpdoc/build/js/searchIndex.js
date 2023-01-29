d4d921501826d632b64d7a4d5b63326b83d81004
Shiina <shiinayusuke01@gmail.com>
2023-01-29 23:19:30 +0900

    create login system.

---
+++ app/Models/.phpdoc/build/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\App\\Models\\User",
+            "name": "User",
+            "summary": "",
+            "url": "classes/App-Models-User.html"
+        },                {
+            "fqsen": "\\App\\Models\\User\u003A\u003A\u0024fillable",
+            "name": "fillable",
+            "summary": "The\u0020attributes\u0020that\u0020are\u0020mass\u0020assignable.",
+            "url": "classes/App-Models-User.html#property_fillable"
+        },                {
+            "fqsen": "\\App\\Models\\User\u003A\u003A\u0024hidden",
+            "name": "hidden",
+            "summary": "The\u0020attributes\u0020that\u0020should\u0020be\u0020hidden\u0020for\u0020serialization.",
+            "url": "classes/App-Models-User.html#property_hidden"
+        },                {
+            "fqsen": "\\App\\Models\\User\u003A\u003A\u0024casts",
+            "name": "casts",
+            "summary": "The\u0020attributes\u0020that\u0020should\u0020be\u0020cast.",
+            "url": "classes/App-Models-User.html#property_casts"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\App\\Models",
+            "name": "Models",
+            "summary": "",
+            "url": "namespaces/app-models.html"
+        },                {
+            "fqsen": "\\App",
+            "name": "App",
+            "summary": "",
+            "url": "namespaces/app.html"
+        }            ]
+);
---
+++ session.sh
@@ -0,0 +1,18 @@
+curl 'https://www.net-menber.com/myp/top/index' \
+  -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'Accept-Language: en,ja;q=0.9' \
+  -H 'Cache-Control: no-cache' \
+  -H 'Connection: keep-alive' \
+  -H 'Cookie: PHPSESSID=57j73v8spva3idk99qfpg5m803' \
+  -H 'Pragma: no-cache' \
+  -H 'Referer: https://www.net-menber.com/myp/card_use/login' \
+  -H 'Sec-Fetch-Dest: document' \
+  -H 'Sec-Fetch-Mode: navigate' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'Sec-Fetch-User: ?1' \
+  -H 'Upgrade-Insecure-Requests: 1' \
+  -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' \
+  -H 'sec-ch-ua: "Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  --compressed
